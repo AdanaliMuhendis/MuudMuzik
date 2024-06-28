badf00278ba78a2268c755223322aa4e17a9ee05
AdanaliMuhendis <134815100+AdanaliMuhendis@users.noreply.github.com>
2024-06-28 17:55:19 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,13 +7,13 @@ admins = {}
 
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
-THUMB_IMG = getenv("THUMB_IMG", "https://graph.org/file/fa5ccd7123f36f9e12592.jpg")
-BOT_NAME = getenv("BOT_NAME", "🎧 Muud Müzik")
+THUMB_IMG = getenv("THUMB_IMG", "https://telegra.ph/Alem-Music-06-01")
+BOT_NAME = getenv("BOT_NAME", "𝐀𝐋𝐄𝐌 𝐌𝐔𝐒𝐈𝐂")
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 BOT_USERNAME = getenv("BOT_USERNAME") 
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "PlutoFederation")
-PLAYLIST_NAME = getenv("PLAYLIST_NAME", "PlutoFm") 
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "SohbetAlemi")
+PLAYLIST_NAME = getenv("PLAYLIST_NAME", "𝐀𝐋𝐄𝐌 𝐌𝐔𝐒𝐈𝐂") 
 ASSISTANT_NAME = getenv("ASSISTANT_NAME")
 OWNER = getenv("OWNER", "PlutoOwner")
 PMPERMIT = getenv("PMPERMIT", "ENABLE")
@@ -22,4 +22,4 @@ PLAYLIST_ID = int(getenv("PLAYLIST_ID"))
 
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5591074473").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5988763828").split()))
